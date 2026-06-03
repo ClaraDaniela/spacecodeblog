827e409516e1d4ef2e3a92295fb995c3a424ed24
Clara Daniela <claracant123@gmail.com>
2026-06-02 23:49:28 -0300

    se modifico books

--- src/pages/books.tsx
+++ src/pages/books.tsx
@@ -5,9 +5,44 @@ import { Main } from '../templates/Main';
 const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
     <Content>
+
+      <h1>
+        Los que recomiendo:
+      </h1>
+      <ul>Fundación-Isaac Asimov</ul>
+      <ul>Yo, robot-Isaac Asimov</ul>
+      <ul>Los robots del amanecer - Isaac Asimov</ul>
+      <ul>El sol desnudo - Isaac Asimov</ul>
+      <ul>Las cavernas de acero - Isaac Asimov</ul>
+      <ul>El fin de la eternidad - Isaac Asimov</ul>
+      <ul>Guía del autoestopista galáctico-Douglas Adams</ul>
+      <ul>Un mundo feliz-Huxley</ul>
+      <ul>1984 - George Orwell</ul>
+      <ul>Rebelión en la granja - George Orwell</ul>
+      <ul>La máquina del tiempo-H.G Wells</ul>
+      <ul>Culquier cuento de Lovecraft porque lo amo</ul>
+      <ul>Los libros de sangre - Clive Barker</ul>
+      <ul>Nuestra parte de noche - Mariana Enriquez</ul>
+      <ul>Dune-Frank Herbert</ul>
+
+      <h1>
+        Los que NO recomiendo:
+      </h1>
+      <ul>Rafaela-Mariana con toda la Furiasse</ul>
+      <ul>El de las sombras del Gray (a menos que quieras leer porno)</ul>
+      <ul>Seda-Alesandro Baricco</ul>
+
+
       <h1>
         Los que estoy leyendo:
       </h1>
+      <ul>El cerebro - David Eagleman</ul>
+
+      <h1>
+        Los que ya me leí (2026):
+      </h1>
+      <ul>El misterrio de salem's lot-Stephen King</ul>
+      <ul>Guía del autoestopista galáctico-Douglas Adams</ul>
       <ul>Dune-Frank Herbert</ul>
 
       <h1>
